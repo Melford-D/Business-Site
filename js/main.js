7595a3465c545749763ae746c2915a315b250161
Melford-D <melfordbirakor@gmail.com>
2020-09-11 17:51:54 +0100

    Uploaded Sass and Scss files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+
+
+menuBtn.addEventListener('click', () => {
+    menu-btn__burger.classList.toggle('.change');
+});
